b088d3c5e451d5cc851d7d935f2d84433689f8fb
worstburrito <138819542+worstburrito@users.noreply.github.com>
2024-12-19 09:49:47 -0600

    Put calculator in a while loop so user can continually use the calculator.

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Simple For Loop Demo
+// Print Hello World 10 Times
+
+/*
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Hello World - {i}");
+}
+*/
+
+// Ask the User How Many Times to Print Hello World
+/*
+Console.Write("\nHow many times would you like to print 'Hello World'?: \n");
+int count = Convert.ToInt32( Console.ReadLine() );
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"Hello World - {i}");
+}
+*/
+
+// Simple While Loop Demo
+/*
+using System.Diagnostics.Metrics;
+
+int count = 0;
+while (count < 10)
+{
+    Console.WriteLine($"Hello World! -> {count}");
+    count += 2;
+}
+*/
+
+// While Loop Challenge
+/*
+int sum = 0;
+int num = 0;
+
+
+while (num != -1)
+{
+    Console.WriteLine("Please enter numbers to be summed up. (-1 to stop loop)");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num != -1)
+    {
+        sum += num;
+    }
+}
+Console.WriteLine($"Sum: {sum}");
+*/
+
+// Do While Loops Demo
+/*
+int sum = 0;
+int num = 0;
+
+do
+{
+    Console.WriteLine("Please enter numbers to be summed up. (-1 to stop loop)");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num != -1)
+    {
+        sum += num;
+    }
+} while (num != -1);
+Console.WriteLine($"Sum: {sum}");
+*/
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -2,56 +2,84 @@
 
 // Weclome Message
 Console.WriteLine("*~*~*~*~ Welcome to the Sample Calculator! *~*~*~*~\n\n");
+Console.WriteLine("\nWould you like to get started? Press 'Enter' continue or 'E' to exit calculator: ");
+string choice = Console.ReadLine();
 
-// Prompt for User Input
-Console.Write("Please enter the first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("\nPlease enter the second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+while (choice != "E")
+{
+    // Show Calculator Operations
+    Console.WriteLine("Here are your calculation choices:\n");
+    Console.WriteLine("Select '1' for addition...");
+    Console.WriteLine("Select '2' for subtraction...");
+    Console.WriteLine("Select '3' for multiplication...");
+    Console.WriteLine("Select '4' for division...");
+    Console.WriteLine("Select '5' for Fibonacci sequence...");
+    Console.Write("\nWhat calculation would you like to make: ");
+    int calculation = Convert.ToInt32(Console.ReadLine());
 
-// Show Calculator Operations
-Console.WriteLine("Here are your calculation choices:\n");
-Console.WriteLine("Select '1' for addition...");
-Console.WriteLine("Select '2' for subtraction...");
-Console.WriteLine("Select '3' for multiplication...");
-Console.WriteLine("Select '4' for division...");
-Console.Write("\nWhat calculation would you like to make: ");
-int calculation = Convert.ToInt32(Console.ReadLine());
+    // Prompt for User Input
+    Console.Write("\nPlease enter the first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
 
-// Decide Which Operations are Needed Based On Selections
-int result = 0;
-string symbol = string.Empty;
+    Console.Write("\nPlease enter the second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (calculation == 1)
-{
-    result = num1 + num2;
-    symbol = "+";
-} else if (calculation == 2)
-{
-    result = num1 - num2;
-    symbol = "-";
-} else if (calculation == 3)
-{
-    result = num1 * num2;
-    symbol = "*";
-} else if (calculation == 4)
-{
-    result = num1 / num2;
-    symbol = "/";
-} else
-{
-    symbol = "MISSING";
-    Console.WriteLine("\nYou did not select a valid calculation.\n");
+    // Decide Which Operations are Needed Based On Selections
+    int result = 0;
+    string symbol = string.Empty;
+
+    if (calculation == 1)
+    {
+        result = num1 + num2;
+        symbol = "+";
+    }
+    else if (calculation == 2)
+    {
+        result = num1 - num2;
+        symbol = "-";
+    }
+    else if (calculation == 3)
+    {
+        result = num1 * num2;
+        symbol = "*";
+    }
+    else if (calculation == 4)
+    {
+        result = num1 / num2;
+        symbol = "/";
+    }
+    else if (calculation == 5)
+    {
+        for (int i = num1; i <= num2; i++)
+        {
+            result += i;
+        }
+    }
+    else
+    {
+        Console.WriteLine("\nYou did not select a valid calculation.\n");
+    }
+
+    // Print Output Decisions
+    if (calculation >= 1 && calculation <= 4)
+    {
+        Console.WriteLine($"\n{num1} {symbol} {num2} = {result}\n");
+    }
+    else if (calculation == 5)
+    {
+        Console.WriteLine($"\nFibonacci result: {result}\n");
+    }
+    else
+    {
+        Console.WriteLine("\nPlease try again.");
+    }
+
+    Console.WriteLine("\nWould you like to continue? Press 'Enter' continue or 'E' to exit calculator: ");
+    choice = Console.ReadLine();
 }
+Console.WriteLine("\n\n*~*~*~*~ THANK YOU for using the Sample Calculator! *~*~*~*~\n\n");
 
-// Print Output Decisions
-if (calculation >= 1 && calculation <= 4)
-{
-    Console.WriteLine($"\n{num1} {symbol} {num2} = {result}\n");
-} else
-{
-    Console.WriteLine("Please try again.");
-}
+
 
 
